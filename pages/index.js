ed54b9301fb171ca6684fe96e254026ed29b3739
Ismail <forwork.ismail@gmail.com>
2022-03-27 22:22:05 +0430

    Add menuItems

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import Image from 'next/image';
 import styles from "./index.module.css";
-import { IoLogoGithub, IoLogoLinkedin, IoMail } from 'react-icons/io5';
+import { IoLogoGithub, IoLogoLinkedin, IoMail, IoLocation } from 'react-icons/io5';
 
 
 
 function HomePage() {
   return <div className={styles.page}>
     <div className={styles.image}>
-      <div style={{ borderRadius: '50% !important', width: '75%', marginBottom: '1rem', marginTop: '1rem' }}>
-        <Image src="/images/profile.png" width={185} height={200} layout="responsive" style={{ borderRadius: '50% !important' }}/>
+      <div style={{ borderRadius: '50% !important', width: '85%', marginBottom: '1rem', marginTop: '1rem' }}>
+        <Image src="/images/profile.png" width={180} height={200} layout="responsive" style={{ borderRadius: '50% !important' }}/>
       </div>
 
 
@@ -17,21 +17,24 @@ function HomePage() {
         <h2 style={{ color: '#8b949e' }}>Full Stack Developer</h2>
       </div>
 
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', width: '85%' }}>
-        <div className={styles.buttonsDiv}><IoLogoGithub size={25}/>&nbsp;<span>@ismailwork</span></div>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', width: '95%' }}>
+        <div className={styles.buttonsDiv}><IoLogoGithub size={25}/>&nbsp;<span>ismailwork</span></div>
         <div className={styles.buttonsDiv}><IoLogoLinkedin size={25} />&nbsp;<span>ismail</span></div>
-        <div className={styles.buttonsDiv}><IoMail size={25}/>&nbsp;<span>forwork.ismail@gmail.com</span>
+        <div className={styles.buttonsDiv}><IoMail size={25}/>&nbsp;<span>forwork.ismail@gmail.com</span></div>
+        <div className={styles.buttonsDiv}><IoLocation size={25}/>&nbsp;<span>Kabul, Afghanistan</span>
         </div>
       </div>
 
     </div>
     <div className={styles.projects}>
-      <h2>CDE</h2>
-      <h2>CDE</h2>
-      <h2>CDE</h2>
-      <h2>CDE</h2>
-      <h2>CDE</h2>
-      <h2>CDE</h2>
+      <div className={styles.menuItems}>
+        <div className={styles.menuItem}>Bio</div>
+        <div className={styles.menuItem}>Work</div>
+        <div className={styles.menuItem}>Projects</div>
+      </div>
+      <div style={{ minHeight: '20rem'}}>
+
+      </div>
     </div>
   </div>
 }
