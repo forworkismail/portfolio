f8eb4e966beb7e4d2b3b048f833d23a2174b666c
Ismail <forwork.ismail@gmail.com>
2022-03-27 13:34:05 +0430

    Create homepage

---
+++ pages/_document.js
@@ -0,0 +1,25 @@
+
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/Dank_Mono.otf"
+            as="font"
+            crossOrigin="anonymous"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import styles from  "./index.module.css";
+
+
+
+function HomePage() {
+  return <div className={styles.page}>
+    <div className={styles.image}>
+      <h1>ABC</h1>
+      <h1>ABC</h1>
+      <h1>ABC</h1>
+      <h1>ABC</h1>
+    </div>
+    <div className={styles.projects}>
+      <h2>CDE</h2>
+      <h2>CDE</h2>
+      <h2>CDE</h2>
+      <h2>CDE</h2>
+      <h2>CDE</h2>
+      <h2>CDE</h2>
+    </div>
+  </div>
+}
+
+export default HomePage
